d637634fc2e5cf26bb337c2600b12bd71e095c4d
iishapathak <2019095900007536@spu.ac.in>
2022-04-20 17:28:09 +0530

    submit practical

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_4
+{
+    internal class Program
+    {
+        class User
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public string Location
+            {
+                get;
+                set;
+            }
+        }
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.Name = "Isha Pathak";
+            u.Location = "Gujarat";
+            Console.WriteLine("Name: " + u.Name);
+            Console.WriteLine("Location: " + u.Location);
+            Console.ReadLine();
+        }
+    }
+}
